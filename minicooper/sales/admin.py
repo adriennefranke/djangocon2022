2ead5ae696d185f8f670487044eda7e7693bc719
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2022-10-03 20:36:54 -0500

    Update README

--- minicooper/sales/admin.py
+++ minicooper/sales/admin.py
@@ -6,3 +6,10 @@ from .models import Sale
 class SaleAdmin(admin.ModelAdmin):
 	list_display = ('car', 'sale_price', 'sale_date', )
 	search_fields = ('sale_date', )
+
+	# def get_search_results(self, request, queryset, search_term):
+	# 	use_distinct = False
+	# 	if not search_term:
+	# 		return queryset, use_distinct
+	# 	else:
+	# 		return self.model.objects.filter(your filters here using search_term), use_distinct
